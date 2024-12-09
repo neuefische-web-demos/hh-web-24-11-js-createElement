26ef53037ac82ac198f49997d4e6a82d66bb0234
Jessica Lörs <jessica.loers@gmail.com>
2024-12-09 11:32:47 +0100

    initial

---
+++ js/index.js
@@ -0,0 +1,3 @@
+const form = document.querySelector('[data-js="card-form"]');
+const textInput = document.querySelector('[data-js="text-input"]');
+const cardContainer = document.querySelector('[data-js="card-container"]');
